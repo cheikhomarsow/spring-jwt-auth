5a9582ceb6b2962863f9e6aaf22daed54b4e204a
Cosinus <cheikh.s@makeba.money>
2021-07-30 16:42:15 +0000

    User and Role repositories

---
+++ src/main/java/com/example/jwtauth/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.example.jwtauth.repository;
+
+import com.example.jwtauth.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
